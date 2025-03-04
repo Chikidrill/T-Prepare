df5bd3012d2a827e9be3d8fdf348ae86a2f76ce1
Chikidrill <dvornikovkd@yandex.ru>
2025-03-04 15:50:37 +0700

    T-Prepare

---
+++ vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <PageHeader />
+    <RouterView />
+  </div>
+</template>
+
+
+<script>
+import PageHeader from "@/components/PageHeader.vue";
+
+export default {
+  components: {
+    PageHeader,
+  },
+};
+</script>
+
+<style></style>
