0d6c1cc6d72204bb1cfc81b4e72c1459bbd1cd95
Chikidrill <dvornikovkd@yandex.ru>
2025-03-04 22:38:34 +0700

    Add popup pattern

---
+++ vue-project/src/components/PopUp.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="popup">
+    <div class="popup__content">
+      <h2>Выбранный предмет: {{ subjectName }}</h2> <!-- Выводим название предмета -->
+      <button class="close-btn" @click="close">Закрыть</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    subjectName: String, // Пропс для получения названия предмета
+  },
+  methods: {
+    close() {
+      this.$emit('close'); // Закрытие попапа
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.popup {
+  background-color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+&__content {
+  background: white;
+  padding: 20px;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.close-btn {
+  padding: 10px 15px;
+  background: @yellow;
+  font-family: @font3;
+  text-align: right;
+  color:@black;
+  border-radius: 20px;
+  border: none;
+  cursor: pointer;
+}
+}
+</style>
