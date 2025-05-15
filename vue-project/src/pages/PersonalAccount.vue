a2c474d4b65945e6dcf5c9e9cef32424c79a4dd5
Chikidrill <dvornikovkd@yandex.ru>
2025-05-15 12:16:39 +0700

    адаптив в личном кабинете

--- vue-project/src/pages/PersonalAccount.vue
+++ vue-project/src/pages/PersonalAccount.vue
@@ -4,7 +4,7 @@
       <div class="profile__introduction">
         <p class="profile__introduction-text">Личный кабинет</p>
         <div class="profile__icon">
-          <svg width="75" height="75" viewBox="0 0 153 153" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg  viewBox="0 0 153 153" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="76.5" cy="76.5" r="76.5" fill="black"/>
             <path d="M116.776 106.073C112.999 99.3588 106.813 91.7541 96.7422 87.431C90.9823 91.4606 83.9849 93.8332 76.4415 93.8332C68.8938 93.8332 61.8965 91.4608 56.1364 87.431C46.0656 91.7541 39.8793 99.359 36.1045 106.073C31.0946 114.978 35.0224 127.578 43.6868 127.578C52.3514 127.578 76.4417 127.578 76.4417 127.578C76.4417 127.578 100.53 127.578 109.194 127.578C117.859 127.578 121.786 114.978 116.776 106.073Z" fill="white"/>
             <path d="M76.4415 85.407C91.2351 85.407 103.224 73.4139 103.224 58.6223V52.206C103.224 37.4146 91.2351 25.4215 76.4415 25.4215C61.6457 25.4215 49.6548 37.4146 49.6548 52.2062V58.6225C49.6548 73.4139 61.6459 85.407 76.4415 85.407Z" fill="white"/>
@@ -151,7 +151,6 @@ mounted() {
   &__introduction-text {
     font: @font1;
     font-size: 60px;
-
   }
   
   &__user-content{
@@ -220,16 +219,33 @@ mounted() {
 
 }
 
-.background-img{
-  position: fixed; 
-    bottom: 0;
-    left: 0;
-    width: 100vw; 
-    height: auto; 
-    overflow: hidden; 
-    display: flex;
+@media @bw768{
+  .profile{
+    margin: 0;
+    padding-left: 95px;
+    padding-right: 95px;
+    &__introduction{
+      align-items: center;
+    }
+    &__introduction-text{
+      font-size: 40px;
+    }
+    &__icon {
+    width: 50px;
+    height: 50px;
+    margin: 0;
     justify-content: center;
-    align-items: center;
-    z-index: -1; 
+  }
+  &__username{
+    font-size: 30px;
+  }
+  &__user-text{
+    font-size: 30px;
+  }
+  &__user-content{
+    gap: 100px;
+  }
+  }
+  
 }
 </style>
